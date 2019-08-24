0c393a82c64cde206ced96153507c1f89bc545d7
James Essex <JMEssex@gmail.com>
2019-08-24 16:18:17 -0500

    Created Footer UI with searchbar and filtering functionality from RouteList + added Chips with Route for selected filtered route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'map', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'map', loadChildren: './map/map.module#MapPageModule' },
+  // { path: 'map', loadChildren: './map/map.module#MapPageModule' }, // TODO: Remove once refactored to new engine IF needed (Aug. 24, 2019)
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { IonSearchbar } from '@ionic/angular';
+
+import { routeList } from '../../data/route-list';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit{
+  @ViewChild('searchBar', {static: true}) searchBar: IonSearchbar;
+  routes = routeList;
+  filteredBusList;
+  routePills: string[] = [];
+  constructor() {
+  }
 
-  constructor() {}
+
+
+  ngAfterViewInit() {
+    this.searchBarInit();
+  }
+
+  searchBarInit() {
+    this.searchBar.ionChange.subscribe((val: any) => {
+      if (val.detail.value !== '') {
+        this.filteredBusList = this.routes.route.filter(route => route.tag.includes(val.detail.value));
+      } else {
+        this.filteredBusList = [];
+      }
+    });
+  }
+
+  addRoutePill(route) {
+    if (this.routePills.length < 3 && this.routePills.indexOf(route.tag) === -1) {
+      this.routePills.push(route.tag);
+    }
+  }
+
+  removeRoutePill(pill: string) {
+    this.routePills.splice(this.routePills.indexOf(pill), 1);
+  }
+
+
 
 }
