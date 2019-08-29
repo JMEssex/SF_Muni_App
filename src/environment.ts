a44d97c6b9eae59d3dc07b4a464c0457589c82f3
James Essex <JMEssex@gmail.com>
2019-08-28 20:27:29 -0500

    added firebase features

---
+++ src/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCOTR-LEwId_tJGE2xY8xrp_bR8I7CY-vw',
+  authDomain: 'sf-muni-app.firebaseapp.com',
+  databaseURL: 'https://sf-muni-app.firebaseio.com',
+  projectId: 'sf-muni-app',
+  storageBucket: '',
+  messagingSenderId: '984114893099',
+  appId: '1:984114893099:web:31d96e1951a68516'
+}
