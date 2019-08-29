428f12a17c401f31d045236faad53bb05df6f1f9
James Essex <JMEssex@gmail.com>
2019-08-28 20:25:44 -0500

    added firebase features

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,13 @@ export const environment = {
   mapbox: {
     accessToken: 'pk.eyJ1Ijoiam1lc3NleCIsImEiOiJjanphZHM4YTMwMW01M2p0bW16OG5xM2J5In0.qfpc0F8TznC6XVanvCt8sw'
   },
+  firebase: {
+    apiKey: 'AIzaSyCOTR-LEwId_tJGE2xY8xrp_bR8I7CY-vw',
+    authDomain: 'sf-muni-app.firebaseapp.com',
+    databaseURL: 'https://sf-muni-app.firebaseio.com',
+    projectId: 'sf-muni-app',
+    storageBucket: '',
+    messagingSenderId: '984114893099',
+    appId: '1:984114893099:web:31d96e1951a68516'
+  }
 };
